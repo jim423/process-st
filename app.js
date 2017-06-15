a9c06cb3b71ebe19b585f08f661ae390795d1ffe
Jim <jim423@live.cn>
2017-06-14 22:21:29 -0400

    bug fixed

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var app = angular.module('myApp', ['blueimp.fileupload']);
 app.controller('mainCtrl', function($scope, $http) {
-	$scope.showVideo = false;
+	var video_id;
+	$scope.count = 0;
 
 	$scope.submit = function() {
-        $scope.showVideo = false;
-
 		$('input').trigger('click');
 	};
 
@@ -12,18 +11,20 @@ app.controller('mainCtrl', function($scope, $http) {
 		url: 'https://upload.wistia.com?access_token='+
 		'88003df5a4da31fd367ae41feba9445d4df24ccee41200abb536284a7957b135&project_id=5sj05sct73',
 		done: function(e, data) {
+			video_id = data.result.hashed_id;
+			$scope.count += 1;
 			$('#progress .bar').css(
 	            'width',
 	            0 + '%'
 	        );
 	        $('#progress .bar').html('');
-			var video_id = data.result.hashed_id;
-			var script = "<script src='https://fast.wistia.com/embed/medias/" + video_id
+			var script = "<script id='" + video_id + "' src='https://fast.wistia.com/embed/medias/" + video_id
 			+ ".jsonp' async><\/script>";
+			$('span.video_' + $scope.count).addClass('wistia_async_' + video_id);
 			$('body').append(script);
-			$('span.wistia_embed').addClass('wistia_async_' + video_id);
+			
 			$scope.$apply(function(){
-	            $scope.showVideo = true;
+	            $scope.count = $scope.count;
 	        });
 		},
 		progress: function (e, data) {
